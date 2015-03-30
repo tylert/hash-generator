c60dbdad1ed08640501ea10d2ef0944c886e22c6
Tyler Tidman <tyler.tidman@draak.ca>
2015-03-30 15:28:49 +0000

    Add in instructions for getting it to run under python3

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+#sudo easy_install pip
+#sudo pip install virtualenv
+#... or...
+#sudo apt-get install python-pip python3-pip
+#sudo apt-get install python-virtualenv python3-virtualenv
+
 if [ ! -d venv ]; then
     virtualenv venv
     source venv/bin/activate
@@ -8,3 +14,7 @@ else
     source venv/bin/activate
     pip install --upgrade --requirement requirements.txt
 fi
+
+#virtualenv venv3 --python=$(which python3)
+#source venv3/bin/activate
+#pip3 install --requirement requirements.txt
